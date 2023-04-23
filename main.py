f783876f87db621a929eebe3e9b74a6741c3a524
AlexNaguy <gorniynaguy@mail.ru>
2023-04-23 17:31:41 +0300

    Реализовал класс Engine

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+class Engine:
+    def __init__(self):
+        self.__state = False
+        self.__oil = False
+
+    @property
+    def state(self):
+        return self.__state
+
+    @state.setter
+    def state(self, value: bool):
+        self.__state = value
+
+    @property
+    def oil(self):
+        return self.__oil
+
+    @oil.setter
+    def oil(self, oil: bool):
+        self.__oil = oil
+
+    def print(self):
+        return "Двигатель заведен" if self.__state else "Двигатель не заведен"
+
+    def oil_print(self):
+        return "Масло в двигателе в пределах нормы" if self.__oil else "Масло не в пределах нормы"
+
+
 
 def execute_application():
 
